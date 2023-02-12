2da76eb911ca329869d2052bc94eb7722ac9799c
Артём <lalala_tra@mail.ru>
2023-02-12 14:32:43 +0500

    Start Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikimedia-logo.png/480px-Wikimedia-logo.png"
+                alt="logo"/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+                     alt="image"/>
+            </div>
+            <div>ava + description</div>
+            <div>
+                my post
+                <div>New Post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
